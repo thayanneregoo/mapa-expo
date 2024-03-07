1ce66d558ae487d6e8ad5d1320c28e5bce6697f8
thayanneregoo <thayanneregoo@hotmail.com>
2024-03-07 20:41:09 -0300

    my first commit

---
+++ App.js
@@ -0,0 +1,22 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import MapView from 'react-native-maps'
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+       <MapView style={styles.map}/>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+   
+  },
+  map:{
+    width:"100%",
+    height:"100%",
+  },
+});
